d0fae161cedeb7557c0b7b24ed362f6cbacf6dbe
DonAntonioJose <don.antonio.jose@gmail.com>
2017-01-23 21:27:37 +0100

    i think that work... i think..

---
+++ js/js-uala.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	if (typeof(homeslider_speed) == 'undefined')
+		homeslider_speed = 500;
+	if (typeof(homeslider_pause) == 'undefined')
+		homeslider_pause = 3000;
+	if (typeof(homeslider_loop) == 'undefined')
+		homeslider_loop = true;
+	if (typeof(homeslider_width) == 'undefined')
+		homeslider_width = 1000;
+});
